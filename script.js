231ce6c326728db2a33eb0cd6a72ca001c200e37
trisafotp <trisafotp@gmail.com>
2017-07-10 06:45:20 -0400

    lab 8 additions

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ var shoppingList = [
     item: 'milk (gallons)',
     price: 1.99,
     quantity: 2
+  },
+  {
+    item: 'spinach (bunches)',
+    price: 0.69,
+    quantity: 8
   }
   ]
 
@@ -27,3 +32,27 @@ shoppingList.forEach(function(item){
 });
 
 var total = 1.06*subtotal
+
+var divi=document.createElement('divi');
+document.body.appendChild(divi);
+var para1=document.createElement('p');
+divi.appendChild(para1);
+para1.innerText= 'Pounds of apples: '+shoppingList[0].quantity+'. Price per pound: $'+shoppingList[0].price+'.';
+var para2=document.createElement('p');
+divi.appendChild(para2);
+para2.innerText="Pound of pears: "+shoppingList[1].quantity+'. Price per pound: $'+shoppingList[1].price+'.';
+var para3=document.createElement('p');
+divi.appendChild(para3);
+para3.innerText="Gallons of milk: "+shoppingList[2].quantity+'. Price per gallon: $'+shoppingList[2].price+'.';
+var para4=document.createElement('p');
+divi.appendChild(para4);
+para4.innerText="Bunches of spinach: "+shoppingList[3].quantity+'. Price per bunch: $'+shoppingList[3].price+'.';
+var para5=document.createElement('p')
+divi.appendChild(para5);
+para5.innerText="Subtotal: $"+subtotal;
+var para6=document.createElement('p');
+divi.appendChild(para6);
+para6.innerText="Sales Tax(6%): $"+.06*subtotal;
+var para7=document.createElement('p');
+divi.appendChild(para7);
+para7.innerText="Total: $"+1.06*subtotal;
